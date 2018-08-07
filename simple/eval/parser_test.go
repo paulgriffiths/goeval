7711171f5bd13cbcdd2fed3fdcb852744be57ddf
Paul Griffiths <mail@paulgriffiths.net>
2018-08-07 08:26:24 -0400

    Simplify parser tests.

--- simple/eval/parser_test.go
+++ simple/eval/parser_test.go
@@ -4,174 +4,55 @@ import (
     "testing"
 )
 
-func TestParseNumber(t *testing.T) {
-    v, err := Evaluate("2")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
+func TestVariousExpressions(t *testing.T) {
+    cases := []struct{
+        expr string
+        want float64
+        err error
+    }{
+        // {"", 99, Empty expression error?},
+        // {"(", 99, UnbalancedParenthesesError},
+        // {"()", 99, Empty expression error?},
+        // {"-1", -1, nil},
+        // {"-1*-3", -3, nil},
+        {"(1", 99, UnbalancedParenthesesError},
+        {")", 99, UnbalancedParenthesesError},
+        {"0", 0, nil},
+        {"1", 1, nil},
+        {"(1)", 1, nil},
+        {"((1))", 1, nil},
+        {"1+2", 3, nil},
+        {"1+2+3", 6, nil},
+        {"1+2+3+4", 10, nil},
+        {"1-2", -1, nil},
+        {"1-2-3", -4, nil},
+        {"1-2-3-4", -8, nil},
+        {"1+2-3", 0, nil},
+        {"1-2+3", 2, nil},
+        {"1*2", 2, nil},
+        {"1*2*3", 6, nil},
+        {"1*2*3*4", 24, nil},
+        {"60/2", 30, nil},
+        {"60/2/3", 10, nil},
+        {"60/2/3/5", 2, nil},
+        {"4/0", 99, DivideByZeroError},
+        {"4*6/2", 12, nil},
+        {"6/3*3", 6, nil},
+        {"1+2*3", 7, nil},
+        {"(1+2)*3", 9, nil},
+        {"1+2*3+4", 11, nil},
+        {"1+(2*3)+4", 11, nil},
+        {"(1+2)*(3+4)", 21, nil},
+        {"((1+2)*(3+4))", 21, nil},
     }
-    if v != 2.0 {
-        t.Errorf("Got %v, want %v", v, 2.0)
-    }
-}
 
-func TestParseParenthesizedNumber(t *testing.T) {
-    v, err := Evaluate("(2)")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 2.0 {
-        t.Errorf("Got %v, want %v", v, 2.0)
-    }
-}
-
-func TestParseDoubleParenthesizedNumber(t *testing.T) {
-    v, err := Evaluate("((2))")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 2.0 {
-        t.Errorf("Got %v, want %v", v, 2.0)
-    }
-}
-
-func TestParseNumberMissingRightParenthesis(t *testing.T) {
-    _, err := Evaluate("(2")
-    if err != UnbalancedParenthesesError {
-        t.Errorf("Got %v, want %v", err, UnbalancedParenthesesError)
-    }
-}
-
-func TestParseRightParenthesis(t *testing.T) {
-    _, err := Evaluate(")")
-    if err != UnbalancedParenthesesError {
-        t.Errorf("Got %v, want %v", err, UnbalancedParenthesesError)
-    }
-}
-
-func TestParseMultiply(t *testing.T) {
-    v, err := Evaluate("2*3")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 6.0 {
-        t.Errorf("Got %v, want %v", v, 6.0)
-    }
-}
-
-func TestParseMultiplyThreeNumbers(t *testing.T) {
-    v, err := Evaluate("2*3*4")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 24.0 {
-        t.Errorf("Got %v, want %v", v, 24.0)
-    }
-}
-
-func TestParseDivide(t *testing.T) {
-    v, err := Evaluate("3/2")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 1.5 {
-        t.Errorf("Got %v, want %v", v, 1.5)
-    }
-}
-
-func TestParseDivideThreeNumbers(t *testing.T) {
-    v, err := Evaluate("60/10/2")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 3.0 {
-        t.Errorf("Got %v, want %v", v, 3.0)
-    }
-}
-
-func TestParseDivideByZero(t *testing.T) {
-    _, err := Evaluate("3/0")
-    if err != DivideByZeroError {
-        t.Errorf("Got %v, want %v", err, DivideByZeroError)
-    }
-}
-
-func TestParseAdd(t *testing.T) {
-    v, err := Evaluate("6+9")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 15.0 {
-        t.Errorf("Got %v, want %v", v, 15.0)
-    }
-}
-
-func TestParseAddThreeNumbers(t *testing.T) {
-    v, err := Evaluate("6+9+12")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 27.0 {
-        t.Errorf("Got %v, want %v", v, 27.0)
-    }
-}
-
-func TestParseAddSubtract(t *testing.T) {
-    v, err := Evaluate("6+9-12")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 3.0 {
-        t.Errorf("Got %v, want %v", v, 3.0)
-    }
-}
-
-func TestParseSubtract(t *testing.T) {
-    v, err := Evaluate("11-5")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 6.0 {
-        t.Errorf("Got %v, want %v", v, 6.0)
-    }
-}
-
-func TestParseSubtractThreeNumber(t *testing.T) {
-    v, err := Evaluate("11-5-18")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != -12.0 {
-        t.Errorf("Got %v, want %v", v, -12.0)
-    }
-}
-
-func TestParseSubtractAdd(t *testing.T) {
-    v, err := Evaluate("6-9+12")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 9.0 {
-        t.Errorf("Got %v, want %v", v, 9.0)
-    }
-}
-
-func TestParseAddAndMultiply(t *testing.T) {
-    v, err := Evaluate("2+3*4")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 14.0 {
-        t.Errorf("Got %v, want %v", v, 14.0)
-    }
-}
-
-func TestParseParenthesizedAddAndMultiply(t *testing.T) {
-    v, err := Evaluate("(2+3)*4")
-    if err != nil {
-        t.Errorf("Got %v, want %v", err, nil)
-    }
-    if v != 20.0 {
-        t.Errorf("Got %v, want %v", v, 20.0)
+    for _, c := range cases {
+        result, err := Evaluate(c.expr)
+        if err != c.err {
+            t.Errorf("Expr '%s': got %v, want %v", c.expr, err, c.err)
+        } else if err == nil && result != c.want {
+            t.Errorf("Expr '%s': got %v, want %v", c.expr, result, c.want)
+        }
     }
 }
 
