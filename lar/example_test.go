f6773bcf8a48d7ff24d7cb31be7081f70efb307e
Paul Griffiths <mail@paulgriffiths.net>
2018-08-08 23:01:26 -0400

    Format with gofmt.

--- lar/example_test.go
+++ lar/example_test.go
@@ -1,47 +1,46 @@
 package lar
 
 import (
-    "fmt"
-    "strings"
+	"fmt"
+	"strings"
 )
 
 func Example() {
 	lar, _ := NewLookaheadReader(strings.NewReader("abc123?"))
 
-    if lar.MatchDigits() {
-        fmt.Printf("Matched digits '%s'.\n", lar.Result())
-    } else {
-        fmt.Printf("Didn't match any digits.\n")
-    }
+	if lar.MatchDigits() {
+		fmt.Printf("Matched digits '%s'.\n", lar.Result())
+	} else {
+		fmt.Printf("Didn't match any digits.\n")
+	}
 
-    if lar.MatchLetters() {
-        fmt.Printf("Matched letters '%s'.\n", lar.Result())
-    } else {
-        fmt.Printf("Didn't match any digits.\n")
-    }
+	if lar.MatchLetters() {
+		fmt.Printf("Matched letters '%s'.\n", lar.Result())
+	} else {
+		fmt.Printf("Didn't match any digits.\n")
+	}
 
-    if lar.MatchDigits() {
-        fmt.Printf("Matched digits '%s'.\n", lar.Result())
-    } else {
-        fmt.Printf("Didn't match any digits.\n")
-    }
+	if lar.MatchDigits() {
+		fmt.Printf("Matched digits '%s'.\n", lar.Result())
+	} else {
+		fmt.Printf("Didn't match any digits.\n")
+	}
 
-    if lar.MatchOneOf('!', '$', '%') {
-        fmt.Printf("Matched character '%s'.\n", lar.Result())
-    } else {
-        fmt.Printf("Didn't match a character.\n")
-    }
+	if lar.MatchOneOf('!', '$', '%') {
+		fmt.Printf("Matched character '%s'.\n", lar.Result())
+	} else {
+		fmt.Printf("Didn't match a character.\n")
+	}
 
-    if lar.MatchOneOf('{', '?', '#', '@') {
-        fmt.Printf("Matched character '%s'.\n", lar.Result())
-    } else {
-        fmt.Printf("Didn't match a character.\n")
-    }
+	if lar.MatchOneOf('{', '?', '#', '@') {
+		fmt.Printf("Matched character '%s'.\n", lar.Result())
+	} else {
+		fmt.Printf("Didn't match a character.\n")
+	}
 
-    // Output: Didn't match any digits.
-    // Matched letters 'abc'.
-    // Matched digits '123'.
-    // Didn't match a character.
-    // Matched character '?'.
+	// Output: Didn't match any digits.
+	// Matched letters 'abc'.
+	// Matched digits '123'.
+	// Didn't match a character.
+	// Matched character '?'.
 }
-
