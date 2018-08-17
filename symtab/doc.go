04be48145d4e109ecac1d38e111b5560161527a4
Paul Griffiths <mail@paulgriffiths.net>
2018-08-17 23:09:50 +0300

    Format with gofmt.

--- symtab/doc.go
+++ symtab/doc.go
@@ -1,5 +1,5 @@
 /*
-Package symtab implements a multi-level symbol table. 
+Package symtab implements a multi-level symbol table.
 
 New scopes can be pushed onto the symbol table to enable shadowing
 of symbols in inner scopes, and lookups of symbols in outer scopes.
