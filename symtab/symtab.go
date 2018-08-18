a12e5e9a9dff32a6ee1e5934e988e61b9b243a35
Paul Griffiths <mail@paulgriffiths.net>
2018-08-18 10:37:17 +0300

    Refactor symbol table.

--- symtab/symtab.go
+++ symtab/symtab.go
@@ -3,43 +3,37 @@ package symtab
 // SymTab implements a multi-level symbol table
 type SymTab struct {
 	tables []map[string]Symbol
-	top    int
 }
 
 // NewTable creates and returns a new, empty symbol table
 func NewTable() *SymTab {
-	var s SymTab
-	s.tables = []map[string]Symbol{make(map[string]Symbol)}
-	s.top = 0
-	return &s
+	return &SymTab{[]map[string]Symbol{make(map[string]Symbol)}}
 }
 
 // Push pushes a new scope onto the symbol table stack
 func (t *SymTab) Push() {
 	t.tables = append(t.tables, make(map[string]Symbol))
-	t.top++
 }
 
 // Pop removes the innermost scope from the symbol table stack
 func (t *SymTab) Pop() {
-	if t.top < 1 {
+	if len(t.tables) == 1 {
 		panic("symbol table stack underflow")
 	}
-	t.tables[t.top] = nil
-	t.top--
+	t.tables[len(t.tables) - 1] = nil
+    t.tables = t.tables[:len(t.tables) - 1]
 }
 
 // Stores a symbol with the specified key
 func (t *SymTab) Store(key string, val Symbol) {
-	t.tables[t.top][key] = val
+	t.tables[len(t.tables) - 1][key] = val
 }
 
 // Retrieves the symbol for the specified key, or a dummy key and
 // false if the key is not present in the symbol table.
 func (t *SymTab) Retrieve(key string) (Symbol, bool) {
-	for n := t.top; n >= 0; n-- {
-		s, ok := t.tables[n][key]
-		if ok {
+	for i := len(t.tables) - 1; i >= 0; i-- {
+		if s, ok := t.tables[i][key]; ok {
 			return s, true
 		}
 	}
