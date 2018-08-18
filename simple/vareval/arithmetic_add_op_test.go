0a3882c38bab4ce9036e114b4c6e0324c8577bca
Paul Griffiths <mail@paulgriffiths.net>
2018-08-18 21:23:00 +0300

    Add unsuccessful add op tests.

---
+++ simple/vareval/arithmetic_add_op_test.go
@@ -0,0 +1,130 @@
+package vareval
+
+import (
+	"testing"
+)
+
+var numberGoodCases = []struct {
+	values []value
+	result value
+}{
+	{[]value{intValue{2}}, intValue{2}},
+	{[]value{realValue{3.5}}, realValue{3.5}},
+	{[]value{intValue{4}, intValue{5}}, intValue{9}},
+	{[]value{intValue{6}, realValue{7.5}}, realValue{13.5}},
+	{[]value{realValue{8.5}, realValue{9}}, realValue{17.5}},
+	{[]value{realValue{10.5}, realValue{11}}, realValue{21.5}},
+	{[]value{intValue{1}, intValue{2}, intValue{3}}, intValue{6}},
+	{[]value{realValue{1.5}, intValue{2}, intValue{3}}, realValue{6.5}},
+	{[]value{intValue{1}, realValue{2.5}, intValue{3}}, realValue{6.5}},
+	{[]value{intValue{1}, intValue{2}, realValue{3.5}}, realValue{6.5}},
+	{[]value{intValue{1}, realValue{2.5}, realValue{3.5}}, realValue{7.0}},
+	{[]value{realValue{1.5}, intValue{2}, realValue{3.5}}, realValue{7.0}},
+	{[]value{realValue{1.5}, realValue{2.5}, intValue{3}}, realValue{7.0}},
+	{[]value{
+		realValue{1.5}, realValue{2.5}, realValue{3.5},
+	}, realValue{7.5}},
+}
+
+func TestSuccessfulNumericAddOperation(t *testing.T) {
+	for n, testCase := range numberGoodCases {
+		var op expr = testCase.values[0]
+		for _, v := range testCase.values[1:] {
+			op = addOp{op, v}
+		}
+		result, err := op.evaluate(nil)
+		if err != nil {
+			t.Errorf("couldn't evaluate addition operation: %v", err)
+			return
+		}
+		valResult, ok := result.(value)
+		if !ok {
+			t.Errorf("couldn't convert result to numberValue")
+			return
+		}
+		if !valResult.equals(testCase.result) {
+			t.Errorf("case %d, got %v, want %v", n+1,
+				valResult, testCase.result)
+		}
+
+	}
+}
+
+var variableGoodCases = []struct {
+	number   value
+	variable value
+	result   value
+}{
+	{intValue{42}, intValue{99}, intValue{141}},
+	{intValue{42}, realValue{99.5}, realValue{141.5}},
+	{realValue{42.5}, intValue{99}, realValue{141.5}},
+	{realValue{42.5}, realValue{99.5}, realValue{142.0}},
+}
+
+func TestSuccessfulVariableAddOperation(t *testing.T) {
+	for n, testCase := range variableGoodCases {
+		table := newTable()
+		table.store("foobar", testCase.variable)
+		op := addOp{testCase.number, variableValue{"foobar"}}
+
+		result, err := op.evaluate(table)
+		if err != nil {
+			t.Errorf("couldn't evaluate addition operation: %v", err)
+			return
+		}
+		valResult, ok := result.(value)
+		if !ok {
+			t.Errorf("couldn't convert result to numberValue")
+			return
+		}
+		if !valResult.equals(testCase.result) {
+			t.Errorf("case %d, got %v, want %v", n+1,
+				valResult, testCase.result)
+		}
+	}
+}
+
+var numberBadCases = []struct {
+	left  value
+	right value
+}{
+	{intValue{42}, boolValue{false}},
+	{stringValue{"commander_jameson"}, realValue{1.52}},
+	{stringValue{"cobra_mark_ii"}, boolValue{true}},
+	{boolValue{false}, boolValue{true}},
+}
+
+func TestUnsuccessfulNumberAddOperation(t *testing.T) {
+	for n, testCase := range numberBadCases {
+		op := addOp{testCase.left, testCase.right}
+		_, err := op.evaluate(nil)
+		if err != TypeError {
+			t.Errorf("case %d, got %v, want %v", n+1,
+				err, TypeError)
+		}
+	}
+}
+
+var variableBadCases = []struct {
+	number   value
+	variable value
+}{
+	{intValue{42}, boolValue{false}},
+	{stringValue{"commander_jameson"}, realValue{1.52}},
+	{stringValue{"cobra_mark_ii"}, boolValue{true}},
+	{boolValue{false}, boolValue{true}},
+}
+
+func TestUnsuccessfulVariableAddOperation(t *testing.T) {
+	for n, testCase := range variableBadCases {
+		table := newTable()
+		table.store("foobar", testCase.variable)
+		op := addOp{testCase.number, variableValue{"foobar"}}
+
+		_, err := op.evaluate(table)
+		if err != TypeError {
+			t.Errorf("case %d, got %v, want %v", n+1,
+				err, TypeError)
+		}
+	}
+}
