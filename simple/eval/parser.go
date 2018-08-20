8a7163e9800fddf4ef62ad2d96fd040d4b72f132
Paul Griffiths <mail@paulgriffiths.net>
2018-08-20 22:40:15 +0300

    Reorganize files.

--- simple/eval/parser.go
+++ simple/eval/parser.go
@@ -8,39 +8,7 @@ import (
 	"github.com/paulgriffiths/goeval/tokens"
 	"math"
 	"strconv"
-	"strings"
 )
-
-// Evaluates the provided simple mathematical expression.
-func Evaluate(expression string) (expr.Value, error) {
-	ch, err := NewLexer(strings.NewReader(expression))
-	if err != nil {
-		return nil, fmt.Errorf("Couldn't create lexer: %v", err)
-	}
-
-	ltc := tokens.NewLTChan(ch)
-	exp, err := getExpr(ltc)
-	if err != nil {
-		return nil, err
-	}
-
-	if !ltc.IsEmpty() {
-		ltc.Flush()
-		return nil, TrailingTokensError
-	}
-
-	result, err := exp.Evaluate(nil)
-	if err != nil {
-		return nil, err
-	}
-
-	value, ok := expr.ToValue(result)
-	if !ok {
-		return nil, UnknownError
-	}
-
-	return value, nil
-}
 
 func getExpr(ltchan *tokens.LTChan) (expr.Expr, error) {
 	var left expr.Expr
