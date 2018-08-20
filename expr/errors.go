7f18eed24e14638195c33608c17ed924c8363d56
Paul Griffiths <mail@paulgriffiths.net>
2018-08-20 17:22:43 +0300

    Refine package errors.

--- expr/errors.go
+++ expr/errors.go
@@ -1,16 +1,11 @@
 package expr
 
-// evalError is the internal error value type.
-type evalError int
+// exprError is the internal error value type.
+type exprError int
 
 // Error values
 const (
-	UnbalancedParenthesesError evalError = iota
-	DivideByZeroError
-	MissingFactorError
-	MissingArgumentError
-	UnknownFunctionError
-	TrailingTokensError
+	DivideByZeroError exprError = iota
 	DomainError
 	TypeError
 	UnknownIdentifierError
@@ -19,19 +14,14 @@ const (
 
 // descs contains string descriptions of each error value
 var descs = [...]string{
-	UnbalancedParenthesesError: "unbalanced parentheses",
-	DivideByZeroError:          "divide by zero",
-	MissingFactorError:         "missing factor",
-	MissingArgumentError:       "missing function argument",
-	UnknownFunctionError:       "unknown function",
-	TrailingTokensError:        "trailing tokens",
-	DomainError:                "domain error",
-	TypeError:                  "type error",
-	UnknownIdentifierError:     "unknown identifier",
-	RangeError:                 "range error",
+	DivideByZeroError:      "divide by zero",
+	DomainError:            "domain error",
+	TypeError:              "type error",
+	UnknownIdentifierError: "unknown identifier",
+	RangeError:             "range error",
 }
 
 // Error returns a string description of an error
-func (e evalError) Error() string {
+func (e exprError) Error() string {
 	return descs[e]
 }
