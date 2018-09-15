c2091d9e6395c4bab92e6d8eb1f2e1cc3540aa75
Paul Griffiths <mail@paulgriffiths.net>
2018-09-14 22:46:44 -0400

    Export line and character fields.

--- lar/file_pos.go
+++ lar/file_pos.go
@@ -2,26 +2,26 @@ package lar
 
 // FilePos represents a line number and character position in a file.
 type FilePos struct {
-	ch, line int
+	Ch, Line int
 }
 
 // clear clears all the fields in a FilePos
 func (f *FilePos) clear() {
-	f.ch, f.line = 0, 0
+	f.Ch, f.Line = 0, 0
 }
 
 // setPos sets the FilePos's character position and line
 func (f *FilePos) setPos(pos FilePos) {
-	f.ch, f.line = pos.ch, pos.line
+	f.Ch, f.Line = pos.Ch, pos.Line
 }
 
 // inc increments the FilePos's character position
 func (f *FilePos) inc() {
-	f.ch++
+	f.Ch++
 }
 
 // inc increments the FilePos's character position
 func (f *FilePos) incLine() {
-	f.ch = 0
-	f.line++
+	f.Ch = 0
+	f.Line++
 }
